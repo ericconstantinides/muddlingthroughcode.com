cee1673be84d2f856ac27d28dfd18913ea510d2c
Eric Constantinides <eric@ericconstantinides.com>
2017-07-01 00:19:23 -0700

    Add sitemap and using Standard linting

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var marked = require('marked')
 var express = require('express')
 const fs = require('fs')
-const escape = require('escape-html')
 const slug = require('slug')
 const Feed = require('feed')
+const sitemapGenerator = require('sitemap')
 var router = express.Router()
 
 const WEBSITE = {
   url: 'https://www.muddlingthroughcode.com',
   name: '{ muddling through code }',
-  description: 'Welcome to { muddling through code }. This is my journey to '
-    + 'learn, grow, and /* occasionally */ muddle through code.',
+  description: 'Welcome to { muddling through code }. This is my journey to ' +
+    'learn, grow, and /* occasionally */ muddle through code.',
   tagline: 'To learn, sometimes you gotta muddle'
 }
 const EMAIL = 'eric@ericconstantinides.com'
@@ -30,12 +30,23 @@ function cleanDate (dateString) {
 }
 function prettyDate (dateString) {
   dateString = cleanDate(dateString)
-  dateObj = new Date(dateString)
-  const monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December']
-  const weekdayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+  let dateObj = new Date(dateString)
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+  const weekdayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+    'Friday', 'Saturday', 'Sunday']
   return `${weekdayNames[dateObj.getDay()]}, ${monthNames[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`
 }
 
+let sitemap = sitemapGenerator.createSitemap({
+  hostname: WEBSITE.url,
+  cacheTime: 600000,  // 600 sec cache period
+  urls: [{url: '/', changefreq: 'weekly', priority: 1.0}]
+})
+router.get('/sitemap.xml', (req, res, next) => {
+  res.header('Content-Type', 'application/xml')
+  res.send(sitemap.toString())
+})
 
 // prepare RSS
 let feed = new Feed({
@@ -49,9 +60,9 @@ let feed = new Feed({
   // updated: new Date(2013,06,14), // optional, default = today
   // generator: 'awesome', // optional, default = 'Feed for Node.js'
   feedLinks: {
-    rss:  WEBSITE.url + '/rss',
+    rss: WEBSITE.url + '/rss',
     json: WEBSITE.url + '/json',
-    atom: WEBSITE.url + '/atom',
+    atom: WEBSITE.url + '/atom'
   },
   author: {
     name: OWNER,
@@ -61,7 +72,6 @@ let feed = new Feed({
 })
 
 feed.addCategory('Node.js')
-feed.addCategory('Node.js')
 feed.addCategory('Express')
 
 let about
@@ -69,26 +79,27 @@ let about
 let posts = require('../content/posts/posts.json')
   .filter(postObj => postObj.active)
   .filter(postObj => new Date(cleanDate(postObj.date)) <= new Date())
-  .sort((a,b) => new Date(cleanDate(b.date)) - new Date(cleanDate(a.date)))
-fs.readFile('./content/about.md','utf8', (err, aboutContent) => {
+  .sort((a, b) => new Date(cleanDate(b.date)) - new Date(cleanDate(a.date)))
+fs.readFile('./content/about.md', 'utf8', (err, aboutContent) => {
   if (err) throw err
   about = marked(aboutContent)
 })
 
 // pull the content out of the files:
-posts.forEach((postObj,index) => {
+posts.forEach((postObj, index) => {
   postObj.slug = slug(postObj.title).toLowerCase()
   fs.readFile(`./content/posts/${postObj.file}`, 'utf8', (err, postText) => {
     if (err) throw err
-    postObj.teaser = marked(teaserBreak(postText,`<p class="more-link__p"><span class="more-link__outer"><a class="more-link" href="/posts/${postObj.slug}">Read On</a></span></p>`))
+    postObj.teaser = marked(teaserBreak(postText,
+      `<p class="more-link__p"><span class="more-link__outer"><a class="more-link" href="/posts/${postObj.slug}">Read On</a></span></p>`))
     postObj.content = marked(postText)
     postObj.dateDisplay = prettyDate(postObj.date)
 
     // set up the primary image
     if (typeof postObj.primaryImage !== 'undefined' && typeof postObj.primaryImage.image !== 'undefined') {
-      if (typeof postObj.primaryImage.containerClass === 'undefined') postObj.primaryImage.containerClass = '';
+      if (typeof postObj.primaryImage.containerClass === 'undefined') postObj.primaryImage.containerClass = ''
     } else {
-      postObj.primaryImage = false;
+      postObj.primaryImage = false
     }
     // add this post into the feed:
     feed.addItem({
@@ -105,10 +116,11 @@ posts.forEach((postObj,index) => {
       date: new Date(cleanDate(postObj.date)),
       image: (postObj.primaryImage ? WEBSITE.url + postObj.primaryImage.image : '')
     })
+    // add to the sitemap
+    sitemap.add({url: '/posts/' + postObj.slug, changefreq: 'weekly', priority: 0.5})
   })
 
-
-  router.get(`/posts/${postObj.slug}`, function(req, res, next) {
+  router.get(`/posts/${postObj.slug}`, (req, res, next) => {
     res.render('post', {
       title: postObj.title + ' ' + WEBSITE.name,
       ogTitle: `${postObj.title}`,
@@ -121,7 +133,7 @@ posts.forEach((postObj,index) => {
   })
 })
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', {
     title: WEBSITE.name + ' ' + WEBSITE.tagline,
     ogTitle: WEBSITE.name,
@@ -134,13 +146,13 @@ router.get('/', function(req, res, next) {
   })
 })
 
-router.get('/rss', function(req, res, next) {
+router.get('/rss', (req, res, next) => {
   res.send(feed.rss2())
 })
-router.get('/json', function(req, res, next) {
+router.get('/json', (req, res, next) => {
   res.send(feed.json1())
 })
-router.get('/atom', function(req, res, next) {
+router.get('/atom', (req, res, next) => {
   res.send(feed.atom1())
 })
 
