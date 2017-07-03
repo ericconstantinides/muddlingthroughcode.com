70bd8c0c74de8ff4cb090fd4429a6da057167b0f
Eric Constantinides <eric@ericconstantinides.com>
2017-07-02 21:52:06 -0700

    wip meh

---
+++ tasks/webpack.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import webpack from 'webpack'
+
+let config = {
+  entry: {
+    main: [
+      './app.js',
+      'webpack/hot/dev-server',
+      'webpack-hot-middleware/client'
+    ]
+  },
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, '../site')
+  },
+  context: path.resolve(__dirname, '../site'),
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+function scripts() {
+  return new Promise(resolve => webpack(config, (err, stats) => {
+    if (err) console.log('Webpack', err)
+    console.log(stats.toString({ /* stats options */ }))
+    resolve()
+  }))
+}
+
+module.exports = { config, scripts }
