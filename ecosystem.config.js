9f7506b48a82ef4b63a255ac1cf578212eaaa68a
Eric Constantinides <eric@ericconstantinides.com>
2018-06-24 19:41:04 -0700

    Update pm2 ecosystem to use eric2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-13-56-120-181.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/TheKeyPairofEric.pem',
+      user: 'eric',
+      host: 'eric2',
+      // key: '~/.ssh/TheKeyPairofEric.pem',
       ref: 'origin/master',
       repo: 'git@github.com:ericconstantinides/muddlingthroughcode.com.git',
-      path: '/home/ubuntu/Sites/muddlingthroughcode.com',
+      path: '/var/www/node/muddlingthroughcode.com',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
